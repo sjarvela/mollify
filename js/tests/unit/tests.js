59a9bb98bfd33241a2dec06eb16ae67914004603
Samuli Järvelä <samuli@jarvela.com>
2014-02-05 13:55:37 +0200

    js tests

--- js/tests/unit/tests.js
+++ js/tests/unit/tests.js
@@ -1,22 +1,40 @@
 $(function () {
+	var roots = [
+		{"id":"r1","root_id":"r1","parent_id":"","name":"root_1","path":"","is_file":false},
+		{"id":"r2","root_id":"r2","parent_id":"","name":"root_2","path":"","is_file":false}
+	];
+	var root1_files = [
+		{"id":"r1f1","root_id":"r1","parent_id":"r1","name":"file1.txt","path":"file1.txt","is_file":true,"size":"2969919","extension":"txt"},
+		{"id":"r1f2","root_id":"r1","parent_id":"r1","name":"file2.pdf","path":"file2.pdf","is_file":true,"size":"12969919","extension":"pdf"}
+	];
 	
     module('basic')
 
-      test('login', function () {
-		  window.fakeService.sessionInfo({"result":{"authenticated":false}});
+      test('login', function () {		  
 		  stop();
+		  
 		  reload().done(function() {
-		  	start();
+			  start();
+			  
+			  equal(window._ajaxRequests.length, 1);
+			  equal(window._ajaxRequests[0].url, "backend/r.php/session/info/");
+			  
 		      ok($("#mollify-login-name").length, 'username');
 		      ok($("#mollify-login-password").length, 'password');
 		      
 		      $("#mollify-login-name").val("user");
 		      $("#mollify-login-password").val("pw");
 		      
-		      window.fakeService.authenticate({"result":{"authenticated":true,"session_id":"152ed577d28645","user_id":"1","username":"admin","user_type":"a","lang":null,"folders":[{"id":"52affca350207","name":"test","group":"","parent_id":null,"root_id":"52affca350207","path":""}],"roots":[{"id":"52affca350207","name":"test","group":"","parent_id":null,"root_id":"52affca350207","path":""}],"features":{descriptions:true}, "permissions":{}}});
-		      equal(window._ajaxRequests.length, 1);
+		      window.service.whenAuthenticate(window.testData.sessionInfo_admin(roots));		      
 		      
-		      window.fakeService.folderInfo("52affca350207", {"result":{"folder":{"id":"52affca350207","root_id":"52affca350207","parent_id":"","name":"test","path":"","is_file":false}, folders:[], "files":[{"id":"52d2b3c748aa8","root_id":"52affca350207","parent_id":"52affca350207","name":"File1.txt","path":"File1.txt","is_file":true,"size":"2969919","extension":"txt"},{"id":"52affca3d39a9","root_id":"52affca350207","parent_id":"52affca350207","name":"File2.pdf","path":"File2.pdf","is_file":true,"size":"46568","extension":"pdf"}],"permissions":{},"data":{},"hierarchy":[{"id":"52affca350207","root_id":"52affca350207","parent_id":"","name":"test","path":"","is_file":false}]}});
+		      window.service.whenFolderInfo("r1", {
+		      	folder: roots[0],
+		      	folders:[],
+		      	files: root1_files,
+		      	permissions:{},
+		      	data:{},
+		      	hierarchy:[ roots[0] ]
+		      });
 		      
 		      $("#mollify-login-button").trigger("click");
 		      equal(window._ajaxRequests.length, 3);
@@ -24,7 +42,30 @@ $(function () {
 		      equal(window._ajaxRequests[1].url, "backend/r.php/session/authenticate/");
 		      equal(window._ajaxRequests[1].data, "{\"username\":\"user\",\"password\":\"cHc=\",\"remember\":false}");
 		      
-		      equal(window._ajaxRequests[2].url, "backend/r.php/filesystem/52affca350207/info/?h=1");
+		      equal(window._ajaxRequests[2].url, "backend/r.php/filesystem/r1/info/?h=1");
+			  ok($("#mollify-mainview-main").length, 'mainview');
+	      });
+      })
+      
+      test('mainview', function () {
+		  window.service.whenSessionInfo(window.testData.sessionInfo_admin(roots));
+	      window.service.whenFolderInfo("r1", {
+	      	folder: roots[0],
+	      	folders:[],
+	      	files: root1_files,
+	      	permissions:{},
+	      	data:{},
+	      	hierarchy:[ roots[0] ]
+	      });
+
+		  stop();
+		  
+		  reload().done(function() {
+		  	start();
+		      equal(window._ajaxRequests.length, 2);
+		      
+		      equal(window._ajaxRequests[0].url, "backend/r.php/session/info/");
+		      equal(window._ajaxRequests[1].url, "backend/r.php/filesystem/r1/info/?h=1");
 			  ok($("#mollify-mainview-main").length, 'mainview');
 	      });
       })
