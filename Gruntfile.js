d910635e37d30dbb24503c34758c01a630d46c46
Samuli Järvelä <samuli@jarvela.com>
2015-01-01 15:16:26 +0200

    2.5.20

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,7 +200,9 @@ module.exports = function(grunt) {
                     '!backend/plugin/FileViewerEditor/viewers/FlexPaper/**',
                     '!backend/plugin/FileViewerEditor/editors/CKEditor/**',
                     '!backend/plugin/Quota/**',
-                    '!backend/plugin/History/**'
+                    '!backend/plugin/History/**',
+                    '!backend/plugin/Watermarking/**',
+                    '!backend/plugin/SendViaEmail/**'
                 ],
                 dest: 'dist/'
             },
--- js/plugins.js
+++ js/plugins.js
@@ -3606,47 +3606,6 @@
     }
 
     /**
-     *  Send via email -plugin
-     **/
-    mollify.plugin.SendViaEmailPlugin = function() {
-        var that = this;
-
-        this.initialize = function() {};
-
-        return {
-            id: "plugin-sendviaemail",
-            initialize: that.initialize,
-
-            itemContextHandler: function(item, ctx, data) {
-                if (!item.is_file) return false;
-                return {
-                    actions: [{
-                        'title-key': 'actionSendViaEmailSingle',
-                        callback: function() {}
-                    }]
-                };
-            },
-            itemCollectionHandler: function(items) {
-                var folder = false;
-                $.each(items, function(i, itm) {
-                    if (!itm.is_file) {
-                        folder = true;
-                        return false;
-                    }
-                });
-                if (folder) return false;
-
-                return {
-                    actions: [{
-                        'title-key': 'actionSendViaEmailMultiple',
-                        callback: function() {}
-                    }]
-                };
-            }
-        };
-    }
-
-    /**
      *  Registration -plugin
      **/
     mollify.plugin.RegistrationPlugin = function() {
